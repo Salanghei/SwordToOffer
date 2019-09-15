70c6218bd425dfdc15b953de34d22497d8c2e377
Salanghei <1805453683@qq.com>
2019-09-15 22:10:11 +0800

    add PrintListFromTailToHead

---
+++ src/cn/edu/hit/ices/yang/problem/PrintListFromTailToHead.java
@@ -0,0 +1,52 @@
+package cn.edu.hit.ices.yang.problem;
+
+import cn.edu.hit.ices.yang.model.ListNode;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class PrintListFromTailToHead {
+    // 方法一：利用三个指针把链表反转，关键是保存断开的节点
+    public ArrayList<Integer> solution1(ListNode listNode) {
+        if(listNode == null){
+            return new ArrayList<Integer>();
+        }
+
+        ListNode head = listNode;
+        ListNode next = listNode.next;
+        while(next != null){  // 将链表反转
+            ListNode temp = next.next;
+            next.next = head;
+            head = next;
+            next = temp;
+        }
+        // 此时listNode的next还指向第二个节点，将其设为null，防止循环
+        listNode.next = null;
+        ArrayList<Integer> result = new ArrayList<>();
+        while(head != null){
+            result.add(head.val);
+            head = head.next;
+        }
+        return result;
+    }
+
+    // 方法二：利用栈
+    public ArrayList<Integer> solution2(ListNode listNode){
+        if(listNode == null){
+            return new ArrayList<Integer>();
+        }
+
+        Stack<Integer> valStack = new Stack<>();
+        ListNode head = listNode;
+        while(head != null){
+            valStack.push(head.val);
+            head = head.next;
+        }
+
+        ArrayList<Integer> result = new ArrayList<>();
+        while(!valStack.empty()){
+            result.add(valStack.pop());
+        }
+        return result;
+    }
+}
