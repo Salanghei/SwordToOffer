957b8030ab419609814daf79d23c404b53050e10
Salanghei <1805453683@qq.com>
2019-09-16 17:52:55 +0800

    add QueueUseStack

--- src/cn/edu/hit/ices/yang/problem/QueueUseStack.java
+++ src/cn/edu/hit/ices/yang/problem/QueueUseStack.java
@@ -2,6 +2,16 @@ package cn.edu.hit.ices.yang.problem;
 
 import java.util.Stack;
 
+/**
+ * 题目描述：
+ * 用两个栈来实现一个队列，完成队列的Push和Pop操作。 队列中的元素为int类型。
+ *
+ * 解题思路：
+ * 两个栈 stack1 和 stack2:
+ * push 动作都在 stack1 中进行，pop 动作在 stack2 中进行
+ * 当 stack2 不为空时，直接 pop
+ * 当 stack2 为空时，先把 stack1 中的元素 pop 出来，push 到 stack2 中，再从 stack2 中 pop 元素。
+ */
 public class QueueUseStack {
     Stack<Integer> stack1 = new Stack<Integer>();  // 用来实现push
     Stack<Integer> stack2 = new Stack<Integer>();  // 用来实现pop
