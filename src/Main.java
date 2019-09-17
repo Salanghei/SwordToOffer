62a23b769286d20ba1d46a640e2f230441b02579
Salanghei <1805453683@qq.com>
2019-09-17 18:35:21 +0800

    add JumpFloor

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import cn.edu.hit.ices.yang.problem.*;
 
 public class Main {
-    private static NumberOfOne numberOfOne = new NumberOfOne();
+    private static JumpFloor jumpFloor = new JumpFloor();
 
     // 测试
     public static void main(String[] args) {
-        System.out.println(numberOfOne.solution(-1));
+        System.out.println(jumpFloor.solution(3));
     }
 }
